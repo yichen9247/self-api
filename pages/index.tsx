ac220d763b293772f0c70e3c373b44d71ea157bb
yichen9247 <865252486@qq.com>
2024-08-08 18:11:22 +0800

    slef-api

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,7 @@ export default function Page() {
       <Head>
         <meta name="og:title" content="Vercel Edge Network" />
         <meta name="og:description" content="Vercel Edge Network" />
-        <meta
-          name="og:image"
-          content={
-            // Because OG images must have a absolute URL, we use the
-            // `VERCEL_URL` environment variable to get the deployment’s URL.
-            // More info:
-            // https://vercel.com/docs/concepts/projects/environment-variables
+        <meta name="og:image" content={
             `${
               process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''
             }/api/vercel`
