ff1ef718eb54ff61f453cdcabd3e03412a5a02ed
yichen9247 <865252486@qq.com>
2024-08-08 18:43:04 +0800

    slef-api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+
 import Head from 'next/head'
 
 export default function Page() {
@@ -6,14 +7,13 @@ export default function Page() {
       <Head>
         <meta name="og:title" content="Vercel Edge Network" />
         <meta name="og:description" content="Vercel Edge Network" />
-        <meta name="og:image" content={
-            `${
-              process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''
-            }/api/vercel`
-          }
-        />
+        <link rel="stylesheet" href="assets/style.css" />
       </Head>
-      <h1>A page with Open Graph Image.</h1>
+      <div className="hello-container">
+          <h1 className="hello-title">欢迎使用</h1>
+          <p className="hello-desc">使用Next.JS和TypeScript构建的高性能Image API</p>
+          <a href="#" title="查看文档" className="hello-button button-doc">查看文档</a>
+      </div>
     </div>
   )
 }
