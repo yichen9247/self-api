49800cc3ee7f479eafb0b46d5524a389402e3baa
yichen9247 <865252486@qq.com>
2024-08-09 12:23:28 +0800

    slef-api

--- pages/api/time.tsx
+++ pages/api/time.tsx
@@ -6,10 +6,12 @@ export const config = {runtime: 'edge'};
 export default async function handler(req: { url: string | URL; }) {
     const now = new Date();
     const url = new URL(req.url);
-    const hours = now.getHours().toString().padStart(2, '0');
     const queryParams = Object.fromEntries(url.searchParams);
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    const chinaDate = new Date(now.getTime() + now.getTimezoneOffset() * 60000 + 8 * 60 * 60 * 1000);
+    const hours = chinaDate.getUTCHours().toString().padStart(2, '0');
+    const minutes = chinaDate.getUTCMinutes().toString().padStart(2, '0');
+    const seconds = chinaDate.getUTCSeconds().toString().padStart(2, '0');
 
   return new ImageResponse((
       <div style={{ fontSize: 128,background: queryParams.back ? queryParams.back : 'white',color: queryParams.color ? queryParams.color : 'black',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',justifyContent: 'center' }}>
