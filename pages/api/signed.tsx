f93e893d6765fcea39c0c10bd67ae3c9d012e304
yichen9247 <865252486@qq.com>
2024-08-11 12:39:20 +0800

    new signed.tsx

--- pages/api/signed.tsx
+++ pages/api/signed.tsx
@@ -2,8 +2,7 @@
 import { ImageResponse } from '@vercel/og'
 
 export const config = {runtime: 'edge'};
-
-const simkaiFont = fetch(new URL('https://fonts.xiaokolomi.cn/fonts/simkai.ttf')).then((res) => res.arrayBuffer());
+const simkaiFont = fetch(new URL('https://fonts.xiaokolomi.cn/fonts/Microsoft/simkai.ttf')).then((res) => res.arrayBuffer());
 
 export default async function handler(req: { url: string | URL; }) {
   const url = new URL(req.url);
@@ -11,7 +10,7 @@ export default async function handler(req: { url: string | URL; }) {
 
   return new ImageResponse((
       <div style={{ fontSize: 256,background: queryParams.back ? queryParams.back : 'transparent',color: queryParams.color ? queryParams.color : 'red',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',justifyContent: 'center' }}>
-        {queryParams.name ? queryParams.name : 'Undfined'}
+        {queryParams.name && queryParams.name}
       </div>
-  ),{ width: queryParams.width ? Number(queryParams.width) : 1200,height: queryParams.height ? Number(queryParams.height) : 600,fonts: [{ name: 'simkaiFont',data: await simkaiFont,style: 'normal'}], });
+  ),{ width: queryParams.width ? Number(queryParams.width) : 1200,height: queryParams.height ? Number(queryParams.height) : 600,fonts: [{ name: 'simkaiFont',data: await simkaiFont,style: 'normal'}] });
 }
--- pages/api/static.tsx
+++ pages/api/static.tsx
@@ -2,6 +2,7 @@
 import { ImageResponse } from '@vercel/og'
 
 export const config = {runtime: 'edge'};
+const HarmonyOSFont = fetch(new URL('https://fonts.xiaokolomi.cn/fonts/HarmonyOS/HarmonyOS.woff')).then((res) => res.arrayBuffer());
 
 export default async function handler(req: { url: string | URL; }) {
   const url = new URL(req.url);
@@ -11,5 +12,5 @@ export default async function handler(req: { url: string | URL; }) {
       <div style={{ fontSize: 128,background: queryParams.back ? queryParams.back : 'white',color: queryParams.color ? queryParams.color : 'black',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',justifyContent: 'center' }}>
         {queryParams.text ? queryParams.text : 'Default Text'}
       </div>
-  ),{ width: queryParams.width ? Number(queryParams.width) : 1200,height: queryParams.height ? Number(queryParams.height) : 600 });
+  ),{ width: queryParams.width ? Number(queryParams.width) : 1200,height: queryParams.height ? Number(queryParams.height) : 600,fonts: [{ name: 'simkaiFont',data: await HarmonyOSFont,style: 'normal'}] });
 }
