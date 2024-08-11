3507ad38eba628c92bc0a3f49a629361fa786a51
yichen9247 <865252486@qq.com>
2024-08-11 12:27:29 +0800

    new signed.tsx

--- pages/api/signed.tsx
+++ pages/api/signed.tsx
@@ -10,8 +10,8 @@ export default async function handler(req: { url: string | URL; }) {
   const queryParams = Object.fromEntries(url.searchParams);
 
   return new ImageResponse((
-      <div style={{ fontSize: 128,background: queryParams.back ? queryParams.back : 'transparent',color: queryParams.color ? queryParams.color : 'red',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',justifyContent: 'center' }}>
-        {queryParams.name ? queryParams.name : 'Default Text'}
+      <div style={{ fontSize: 256,background: queryParams.back ? queryParams.back : 'transparent',color: queryParams.color ? queryParams.color : 'red',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',justifyContent: 'center' }}>
+        {queryParams.name ? queryParams.name : 'Default Name'}
       </div>
   ),{ width: queryParams.width ? Number(queryParams.width) : 1200,height: queryParams.height ? Number(queryParams.height) : 600,fonts: [{ name: 'simkaiFont',data: await simkaiFont,style: 'normal'}], });
 }
