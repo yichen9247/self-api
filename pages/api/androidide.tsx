15c67b1a4b9e28038f3c47ae1eb93ec94c68526a
yichen9247 <865252486@qq.com>
2024-08-15 18:09:21 +0800

    add github.tsx

--- pages/api/androidide.tsx
+++ pages/api/androidide.tsx
@@ -1,5 +1,4 @@
 
-
 import axios from 'axios'
 import { ImageResponse } from '@vercel/og'
 
@@ -7,19 +6,19 @@ export const config = {runtime: 'edge'};
 const HarmonyOSFont = fetch(new URL('https://fonts.xiaokolomi.cn/fonts/HarmonyOS/HarmonyOS.woff')).then((res) => res.arrayBuffer());
 
 export default async function handler() {
-  return axios.get('https://api.pro.androidide.cn/config').then(async (data) => {
-    if (data.data.code === 200) {
-        return new ImageResponse((
-            <div style={{ fontSize: 128,background: 'white',color: 'green',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',justifyContent: 'center' }}>后端连接成功</div>
-        ),{ width: 1200,height: 600,fonts: [{ name: 'simkaiFont',data: await HarmonyOSFont,style: 'normal'}] });
-    } else {
+    return axios.get('https://api.pro.androidide.cn/config').then(async (data) => {
+        if (data.data.code === 200) {
+            return new ImageResponse((
+                <div style={{ fontSize: 128,background: 'white',color: 'green',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',justifyContent: 'center' }}>后端连接成功</div>
+            ),{ width: 1200,height: 600,fonts: [{ name: 'simkaiFont',data: await HarmonyOSFont,style: 'normal'}] });
+        } else {
+            return new ImageResponse((
+                <div style={{ fontSize: 128,background: 'white',color: '#dd524d',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',justifyContent: 'center' }}>后端连接失败</div>
+            ),{ width: 1200,height: 600,fonts: [{ name: 'simkaiFont',data: await HarmonyOSFont,style: 'normal'}] });
+        }
+    }).catch(async () => {
         return new ImageResponse((
             <div style={{ fontSize: 128,background: 'white',color: '#dd524d',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',justifyContent: 'center' }}>后端连接失败</div>
         ),{ width: 1200,height: 600,fonts: [{ name: 'simkaiFont',data: await HarmonyOSFont,style: 'normal'}] });
-    }
-  }).catch(async () => {
-    return new ImageResponse((
-        <div style={{ fontSize: 128,background: 'white',color: '#dd524d',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',justifyContent: 'center' }}>后端连接失败</div>
-    ),{ width: 1200,height: 600,fonts: [{ name: 'simkaiFont',data: await HarmonyOSFont,style: 'normal'}] });
-  });
+    });
 }
