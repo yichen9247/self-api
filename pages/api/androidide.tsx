1c4ce72e6462c896abea56a988b54d55114cebc9
yichen9247 <865252486@qq.com>
2024-08-15 16:58:10 +0800

    add github.tsx

--- pages/api/androidide.tsx
+++ pages/api/androidide.tsx
@@ -10,17 +10,17 @@ export default async function handler(req: { url: string | URL; }) {
   const url = new URL(req.url);
   const queryParams = Object.fromEntries(url.searchParams);
 
-  return axios.get('https://api.pro.androidide.cn/config').then((data) => {
+  return axios.get('https://api.pro.androidide.cn/config').then(async (data) => {
     if (data.data.code === 200) {
         return new ImageResponse((
             <div style={{ fontSize: 128,background: 'white',color: 'green',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',justifyContent: 'center' }}>后端连接成功</div>
-        ),{ width: queryParams.width ? Number(queryParams.width) : 1200,height: queryParams.height ? Number(queryParams.height) : 600, });
+        ),{ width: queryParams.width ? Number(queryParams.width) : 1200,height: queryParams.height ? Number(queryParams.height) : 600,fonts: [{ name: 'simkaiFont',data: await HarmonyOSFont,style: 'normal'}] });
     } else {
         return new ImageResponse((
             <div style={{ fontSize: 128,background: 'white',color: '#dd524d',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',justifyContent: 'center' }}>后端连接失败</div>
-        ),{ width: queryParams.width ? Number(queryParams.width) : 1200,height: queryParams.height ? Number(queryParams.height) : 600, });
+        ),{ width: queryParams.width ? Number(queryParams.width) : 1200,height: queryParams.height ? Number(queryParams.height) : 600,fonts: [{ name: 'simkaiFont',data: await HarmonyOSFont,style: 'normal'}] });
     }
-  }).catch(() => {
+  }).catch(async () => {
     return new ImageResponse((
         <div style={{ fontSize: 128,background: 'white',color: '#dd524d',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',justifyContent: 'center' }}>后端连接失败</div>
     ),{ width: queryParams.width ? Number(queryParams.width) : 1200,height: queryParams.height ? Number(queryParams.height) : 600,fonts: [{ name: 'simkaiFont',data: await HarmonyOSFont,style: 'normal'}] });
