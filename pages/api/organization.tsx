306ee4e48134fd65e2017e0362350998bc50e92a
yichen9247 <865252486@qq.com>
2024-08-16 15:31:52 +0800

    add support.tsx

--- pages/api/organization.tsx
+++ pages/api/organization.tsx
@@ -5,15 +5,25 @@ import { ImageResponse } from '@vercel/og'
 export const config = {runtime: 'edge'};
 const HarmonyOSFont = fetch(new URL('https://fonts.xiaokolomi.cn/fonts/HarmonyOS/HarmonyOS.woff')).then((res) => res.arrayBuffer());
 
-export default async function handler(req: { url: string | URL; }) {
+export default async function handler() {
     return axios.get('https://api.github.com/orgs/AndroidIDE-CN/members').then(async (data) => {
-        console.log(data.data)
+        let elements = [];
+        const getElement = () => {
+            for (let i = 0; i < data.data.length; i++) {
+                elements.push(
+                    <img key={i} src={data.data[i].avatar_url} style={{ width: 72, height: 72, borderRadius: '50%', margin: '0 10px', marginBottom: '15px' }} />
+                )
+            }
+            return elements;
+        }
+
         return new ImageResponse((
-            <div style={{ fontSize: 48,background: 'white',color: 'green',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',justifyContent: 'center' }}>
-                {data.data[0]}
+            <div style={{ fontSize: 48,background: 'white',color: '#dd524d',width: '100%',height: '100%',display: 'flex',textAlign: 'center',alignItems: 'center',flexDirection: 'column' }}>
+                <h3>AIDE PRO团队成员</h3>
+                <div style={{ width: '95%', display: 'flex', alignItems: 'center', justifyContent: 'center',margin: '0 auto', flexWrap: 'wrap' }}>
+                    {getElement()}
+                </div>
             </div>
-        ),{ width: 1200,height: 600, });
+        ),{ width: 1200,height: 600,fonts: [{ name: 'simkaiFont',data: await HarmonyOSFont,style: 'normal'}] });
     });
 }
-
-// fonts: [{ name: 'simkaiFont',data: await HarmonyOSFont,style: 'normal'}]
