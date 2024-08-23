1e73817c16459ba91c5e76258d316b0ab2820690
yichen9247 <865252486@qq.com>
2024-08-23 20:39:25 +0800

    update

--- pages/api/workstation.tsx
+++ pages/api/workstation.tsx
@@ -5,7 +5,7 @@ const noticeList = [
     {
         title: '逐梦网站试运营公告',
         cover: 'https://files.superbed.cn/proxy/7468686c6f2633337a7570796f326f696c796e7e7978327f72336f68736e793375717d7b796f332b28337878332a2a7f24242c797d78257f2f2c2b7e2b7925252b2b28787832766c7b',
-        author: '逐梦平台运营',
+        author: '逐梦网站运营',
         avatar: 'https://q2.qlogo.cn/g?b=qq&s=640&nk=2169347942',
         create: '2024-08-23 00:00',
         content: `
