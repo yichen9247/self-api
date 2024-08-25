be24012963188d7aa75c46ae0232aa3decf51f14
yichen9247 <865252486@qq.com>
2024-08-25 22:27:55 +0800

    update

---
+++ pages/api/msi/store/query.tsx
@@ -0,0 +1,22 @@
+
+import storeDetail from "../../../../scripts/storeDetail"
+
+export const config = {runtime: 'edge'};
+
+export default async function handler(req: { url: string | URL; }, res: any) {
+    const url = new URL(req.url);
+    const queryParams = Object.fromEntries(url.searchParams);;
+
+    if (queryParams.type == 'store') {
+        const returnResult = storeDetail.find(item => item.id == queryParams.id) ? { code: 200, data: storeDetail.find(item => item.id == queryParams.id) } : { code: 200, data: '404' };
+        return new Response(JSON.stringify(returnResult), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+        });
+    } else {
+        return new Response(JSON.stringify({ code: 400, message: 'Bad Request' }), {
+            status: 400,
+            headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+        });
+    }
+}
---
+++ scripts/store.js
@@ -0,0 +1,28 @@
+
+export default [
+    {
+        id: 4,
+        name: '音频制作与处理',
+        price: '2.00',
+        cover: 'https://pic.imgdb.cn/item/66cb0d3cd9c307b7e9fa140f.png',
+        disable: false,
+    }, {
+        id: 3,
+        name: '网易云黑胶（年卡）',
+        price: '75.00',
+        cover: 'https://pic.imgdb.cn/item/66cb1636d9c307b7e9092500.png',
+        disable: false,
+    }, {
+        id: 2,
+        name: '网易音乐人（入驻）',
+        price: '10.00',
+        cover: 'https://pic.imgdb.cn/item/66cb16c0d9c307b7e90984f3.png',
+        disable: false,
+    }, {
+        id: 1,
+        name: '腾讯音乐人（入驻）',
+        price: '20.00',
+        cover: 'https://pic.imgdb.cn/item/66cb009dd9c307b7e9e8cf26.png',
+        disable: true,
+    }
+]
