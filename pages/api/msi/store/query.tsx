b47f8800e9646d70a193d27baaf35092fa18286e
yichen9247 <865252486@qq.com>
2024-08-25 22:47:57 +0800

    update

--- pages/api/msi/store/query.tsx
+++ pages/api/msi/store/query.tsx
@@ -3,18 +3,9 @@ import storeDetail from "../../../../scripts/storeDetail"
 
 export const config = {runtime: 'edge'};
 
-export default async function handler(req: any, res: any) {
+export default async function handler(req: { url: string | URL; }, res: any) {
     const url = new URL(req.url);
     const queryParams = Object.fromEntries(url.searchParams);
-
-    const allowedDomain = 'ai.music.xiaokolomi.cn';
-    const origin = req.headers.get('origin');
-    if (origin !== allowedDomain) {
-        return new Response(JSON.stringify({ code: 403, message: 'Forbidden' }), {
-            status: 403,
-            headers: { 'Content-Type': 'application/json' }
-        });
-    }
 
     if (queryParams.type == 'store') {
         const returnResult = storeDetail.find(item => item.id == queryParams.id) ? { code: 200, data: storeDetail.find(item => item.id == queryParams.id) } : { code: 200, data: '404' };
@@ -25,7 +16,7 @@ export default async function handler(req: any, res: any) {
     } else {
         return new Response(JSON.stringify({ code: 400, message: 'Bad Request' }), {
             status: 400,
-            headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+            headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*ai.music.xiaokolomi.cn*' }
         });
     }
 }
--- scripts/apiConfig.js
+++ scripts/apiConfig.js
@@ -1,5 +1,6 @@
 
 export default {
+    api_org: "ai.music.xiaokolomi.cn",
     app_api: "https://music.tiangong.cn/api/sm/",
     app_cos: "https://music-static-cos.tiangong.cn/",
 }
