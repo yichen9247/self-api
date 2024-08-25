16ec3952ae4657617b76eccfa832945b454291af
yichen9247 <865252486@qq.com>
2024-08-25 23:01:11 +0800

    update

--- pages/api/msi/store/query.tsx
+++ pages/api/msi/store/query.tsx
@@ -1,22 +1,30 @@
-
 import storeDetail from "../../../../scripts/storeDetail"
 
 export const config = {runtime: 'edge'};
 
-export default async function handler(req: { url: string | URL; }, res: any) {
+export default async function handler(req, res) {
     const url = new URL(req.url);
     const queryParams = Object.fromEntries(url.searchParams);
 
     if (queryParams.type == 'store') {
-        const returnResult = storeDetail.find(item => item.id == queryParams.id) ? { code: 200, data: storeDetail.find(item => item.id == queryParams.id) } : { code: 200, data: '404' };
+        const store = storeDetail.find(item => item.id === queryParams.id);
+        const returnResult = store ? { code: 200, data: store } : { code: 404, message: 'Not Found' };
         return new Response(JSON.stringify(returnResult), {
-            status: 200,
-            headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+            status: returnResult.code,
+            headers: {
+                'Content-Type': 'application/json',
+                // 只允许特定的域名访问
+                'Access-Control-Allow-Origin': 'http://ai.music.xiaokolomi.cn'
+            }
         });
     } else {
         return new Response(JSON.stringify({ code: 400, message: 'Bad Request' }), {
             status: 400,
-            headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*ai.music.xiaokolomi.cn*' }
+            headers: {
+                'Content-Type': 'application/json',
+                // 这里可以保持通配符，因为400错误响应不需要限制访问
+                'Access-Control-Allow-Origin': '*'
+            }
         });
     }
 }
